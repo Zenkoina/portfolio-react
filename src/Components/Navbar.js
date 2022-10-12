6cd6b30267808794df9913ad57e9613758235739
Zenkoina <fyleopeo@gmail.com>
2022-10-12 08:05:07 +0200

    Init

---
+++ src/Components/Navbar.js
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+    return (
+        <div class="navbar">
+            <nav>
+                <ul>
+                    <li><a href="#Home">Home</a></li>
+                    <li><a href="#Skills">Skills</a></li>
+                    <li><a href="#Projects">Projects</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
